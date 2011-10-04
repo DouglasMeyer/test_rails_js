89243d3e489edca1aa9f5f68e6ab95ca7081db28
Douglas Meyer <Douglas.Meyer@Mail.com>
2011-10-04 10:40:31 -0500

    Don't try to load resources that aren't there. Version bump, seems stable.

--- app/helpers/js_tests_helper.rb
+++ app/helpers/js_tests_helper.rb
@@ -14,4 +14,16 @@ module JsTestsHelper
       acc
     end
   end
+
+  def stylesheet_link_tag_if_exists source
+    if Rails.application.assets.find_asset("#{source}.css")
+      stylesheet_link_tag source
+    end
+  end
+
+  def javascript_include_tag_if_exists source
+    if Rails.application.assets.find_asset("#{source}.js")
+      javascript_include_tag source
+    end
+  end
 end
